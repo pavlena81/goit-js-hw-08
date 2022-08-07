b283ddc150d32d5f85eaa72410cfa0a48d5790b7
Olena Nesteruk <pavlena81@gmail.com>
2022-08-07 23:56:05 +0300

    adds part of twoTask

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,45 @@
+// Ознакомься с документацией библиотеки Vimeo плеера.
+
+// Добавь библиотеку как зависимость проекта через npm.
+
+// Инициализируй плеер в файле скрипта как это описано в 
+// секции pre - existing player, но учти что у тебя плеер 
+// добавлен как npm пакет, а не через CDN.
+
+// Разбери документацию метода on() и начни отслеживать событие timeupdate -
+//     обновление времени воспроизведения.
+
+// Сохраняй время воспроизведения в локальное хранилище. 
+
+// Пусть ключом для хранилища будет строка "videoplayer-current-time".
+
+// При перезагрузке страницы воспользуйся методом setCurrentTime() 
+// для того чтобы возобновить воспроизведение с сохраненной позиции.
+
+// Добавь в проект бибилотеку lodash.throttle и сделай так, чтобы время воспроизведения
+//  обновлялось в хранилище не чаще чем раз в секунду.
+// setItem(key, value) - делает новую, или обновляет уже существующую запись в хранилище.
+
+    // import Vimeo from '@vimeo/player';
+    const iframe = document.querySelector('iframe');
+    
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+
+
+// player.addEventListener('timeupdate', (event) => {
+//   console.log('The currentTime attribute has been updated. Again.');
+// });
+
+// localStorage.setItem("videoplayer-current-time", "value")
+
 
